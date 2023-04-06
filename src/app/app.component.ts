33c267afc7c5d813bf65269f9dde1c21c882d8af
Stefan Starosta <stefan.starosta@gmail.com>
2023-04-06 07:25:09 +0200

    added switching app states

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ import {catchError} from "rxjs/operators";
 export class AppComponent implements OnInit {
   title = 'server-manager-frontend-application';
   appState$: Observable<AppState<CustomResponse>>;
+  readonly DataState = DataState;
 
   constructor(private serverService: ServerService) {
   };
